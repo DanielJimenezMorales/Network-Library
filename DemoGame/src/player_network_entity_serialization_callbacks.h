fc77ad877d9623a90b8a7046362f5a59781e4533
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2024-12-27 12:54:10 +0100

    Fix

--- DemoGame/src/player_network_entity_serialization_callbacks.h
+++ DemoGame/src/player_network_entity_serialization_callbacks.h
@@ -7,6 +7,6 @@ namespace NetLib
 	class Buffer;
 }
 
-static void SerializeForOwner( const GameEntity& entity, NetLib::Buffer& buffer );
-static void SerializeForNonOwner( const GameEntity& entity, NetLib::Buffer& buffer );
-static void DeserializeForOwner( GameEntity& entity, NetLib::Buffer& buffer );
+void SerializeForOwner( const GameEntity& entity, NetLib::Buffer& buffer );
+void SerializeForNonOwner( const GameEntity& entity, NetLib::Buffer& buffer );
+void DeserializeForOwner( GameEntity& entity, NetLib::Buffer& buffer );
