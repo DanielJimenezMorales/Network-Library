716dcfd5db31d96269dbd0fa8f922b28a98f0108
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2025-05-30 22:54:25 +0200

    now each project has its own premake file

---
+++ TestDemoGame/test_game_premake5.lua
@@ -0,0 +1,70 @@
+local project_data = PROJECT_DATA.TEST_GAME
+local common_project_data = PROJECT_DATA.COMMON
+local engine_project_data = PROJECT_DATA.ENGINE
+local network_library_project_data = PROJECT_DATA.NETWORK_LIBRARY
+local client_game_project_data = PROJECT_DATA.CLIENT_GAME
+local server_game_project_data = PROJECT_DATA.SERVER_GAME
+
+project (project_data.NAME)
+	kind "ConsoleApp"
+	location (project_data.PATH)
+	language "C++"
+	targetdir (project_data.PATH .. "bin/")
+	targetname (project_data.NAME .. "_%{cfg.buildcfg}")
+
+	files
+	{
+		ROOT_PATH "vendor/googletest/googletest/src/gtest-all.cc",
+
+		project_data.PATH .. "src/**.h",
+		project_data.PATH .. "src/**.cpp"
+	}
+
+	removefiles
+	{
+		ROOT_PATH "DemoGame/src/launcher/main.cpp"
+	}
+
+	includedirs
+	{
+		project_data.PATH .. "src",
+		common_project_data.PATH .. "src",
+		engine_project_data.PATH .. "src",
+		network_library_project_data.PATH .. "src",
+		client_game_project_data.PATH .. "src",
+		server_game_project_data.PATH .. "src",
+
+		ROOT_PATH "vendor/entt/include/",
+		ROOT_PATH "vendor/json/include/",
+
+		ROOT_PATH "vendor/sdl2/SDL2-2.30.1/include/",
+		ROOT_PATH "vendor/sdl2/SDL2_image-2.8.2/include/",
+		ROOT_PATH "vendor/sdl2/SDL2_mixer-2.8.0/include/",
+		ROOT_PATH "vendor/sdl2/SDL2_ttf-2.22.0/include/",
+
+		ROOT_PATH "vendor/googletest/googletest/include",
+		ROOT_PATH "vendor/googletest/googletest"
+	}
+
+	libdirs
+	{
+		ROOT_PATH "vendor/sdl2/SDL2-2.30.1/lib/x64",
+		ROOT_PATH "vendor/sdl2/SDL2_image-2.8.2/lib/x64",
+		ROOT_PATH "vendor/sdl2/SDL2_mixer-2.8.0/lib/x64",
+		ROOT_PATH "vendor/sdl2/SDL2_ttf-2.22.0/lib/x64"
+	}
+
+	links
+	{
+		client_game_project_data.NAME,
+		server_game_project_data.NAME,
+		common_project_data.NAME,
+		engine_project_data.NAME,
+		network_library_project_data.NAME,
+
+		ROOT_PATH "vendor/sdl2/SDL2-2.30.1/lib/x64/SDL2",
+		ROOT_PATH "vendor/sdl2/SDL2-2.30.1/lib/x64/SDL2main",
+		ROOT_PATH "vendor/sdl2/SDL2_image-2.8.2/lib/x64/SDL2_image",
+		ROOT_PATH "vendor/sdl2/SDL2_mixer-2.8.0/lib/x64/SDL2_mixer",
+		ROOT_PATH "vendor/sdl2/SDL2_ttf-2.22.0/lib/x64/SDL2_ttf"
+	}
